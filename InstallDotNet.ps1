406ced92e0ee889b5241fe9ffc98e6d08ec8a1fd
linuxx <cbeagle@gmail.com>
2020-08-19 18:28:03 -0400

    Formatting

--- InstallDotNet.ps1
+++ InstallDotNet.ps1
@@ -1,15 +1,20 @@
-#This will install .Net 3.5 for the version of windows you are using
+#This will install .Net 3.5 for Windows 10
 
+#Specify a temporary location 
 $strTemDir = $env:TEMP + "\DotNet3.5Install\"
 
+#create a directory in that location to hold our files
 New-Item $strTemDir -ItemType Directory
 
+#Detect OS version
 $strOSVersion = (get-computerinfo osVersion).osVersion
 
+#Download both files
 wget https://github.com/linuxx/DotNet-3.5-Installer/raw/master/Versions/$strOSVersion/Microsoft-Windows-NetFx3-OnDemand-Package~31bf3856ad364e35~amd64~en-US~.cab -OutFile $strTemDir\Microsoft-Windows-NetFx3-OnDemand-Package~31bf3856ad364e35~amd64~en-US~.cab
 wget https://github.com/linuxx/DotNet-3.5-Installer/raw/master/Versions/$strOSVersion/Microsoft-Windows-NetFx3-OnDemand-Package~31bf3856ad364e35~amd64~~.cab -OutFile $strTemDir\Microsoft-Windows-NetFx3-OnDemand-Package~31bf3856ad364e35~amd64~~.cab 
 
+#Install the feature 
 dism.exe /online /enable-feature /featurename:NetFX3 /All /Source:$strTemDir /LimitAccess
 
+#Clean up!
 Remove-Item -path $strTemDir -Recurse -Force
-
--- README.md
+++ README.md
@@ -1,3 +1,14 @@
+# .Net 3.5 installer for Windows 10
+This script will install .net 3.5 on Windows 10. Works on the following Windows 10 versions:
+OS Build | Version | Supported
+------------- | -------------
+Windows 10.0.17134  | 1803 | :heavy_check_mark:
+Windows 10.0.17763  | 1809 | :heavy_check_mark:
+Windows 10.0.18362  | 1903 | :heavy_check_mark:
+Windows 10.0.18363 | 1909 | :heavy_check_mark:
+Windows 10.0.19041 | 2004 | :heavy_check_mark:
+
+
 In an elevated powershell prompt, run the following commands:
 
 * cd \\
@@ -11,3 +22,17 @@ Look to see the version of .net installed
 
 Make sure you have unrestricted ExecutionPolicy enabled
 * Set-ExecutionPolicy Unrestricted
+
+
+## FAQ
+Q: Will it work on Windows (not 10) 
+A: No
+
+Q: Will it work on Server
+A: Dont know
+
+Q: It says that the script isn't signed
+A: Set your execution policy
+
+Q: My Windows 10 version is older, and not listed, will it work?
+A: No, upgrade 
